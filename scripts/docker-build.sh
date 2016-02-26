ef2e377ead94856bcd4e81324ef4875a2fc1e5e2
Geoffrey Chandler <geoffc@gmail.com>
2016-02-26 12:44:33 -0800

    export vars in the init and allow dashes in the project name

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,4 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/docker-init.sh
 
-PROJECT_VERSION=`grep Project-Version build/tmp/jar/MANIFEST.MF | awk '{print $2}' | tr -dc '[:alnum:]\.'`
-PROJECT_NAME=`grep Project-Name build/tmp/jar/MANIFEST.MF | awk '{print $2}' | tr -dc '[:alnum:]\.'`
-
 docker build -t rbmh-docker.docker.rbmhops.net/$PROJECT_NAME:$PROJECT_VERSION build/
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,9 +2,5 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/docker-init.sh
 
-# Get the project version from the manifest that we generated when making the jar.
-PROJECT_VERSION=`grep Project-Version build/tmp/jar/MANIFEST.MF | awk '{print $2}' | tr -dc '[:alnum:]\.'`
-PROJECT_NAME=`grep Project-Name build/tmp/jar/MANIFEST.MF | awk '{print $2}' | tr -dc '[:alnum:]\.'`
-
 # Finally we can push the tagged image
 docker push rbmh-docker.docker.rbmhops.net/${PROJECT_NAME}:${PROJECT_VERSION}
