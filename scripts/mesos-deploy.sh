d7899886bb5d57fc123b727b7fbb7868fd279d9d
Geoffrey Chandler <geoffc@gmail.com>
2016-02-26 19:54:08 -0800

    temp workaround for staging mesos deploy

--- scripts/mesos-deploy.sh
+++ scripts/mesos-deploy.sh
@@ -8,7 +8,7 @@ MESOS_URL="http://mesos-master1.rbss.staging.rbmhops.net:8080/v2/apps"
 RESPONSE=`curl -I $MESOS_URL/$MARATHON_APP_ID 2>/dev/null | head -n 1 | awk -F" " '{print $2}'`
 
 if [ 200 -eq $RESPONSE ]; then
-	curl -X PUT -H "Content-Type: application/json" $MESOS_URL/$MARATHON_APP_ID -d @"build/mesos.deploy.json"
+	curl -X PUT -H "Content-Type: application/json" $MESOS_URL/$MARATHON_APP_ID -d @"build/mesos-deploy-staging.json"
 else
-	curl -X POST -H "Content-Type: application/json" $MESOS_URL -d @"build/mesos.deploy.json"
+	curl -X POST -H "Content-Type: application/json" $MESOS_URL -d @"build/mesos-deploy-staging.json"
 fi
