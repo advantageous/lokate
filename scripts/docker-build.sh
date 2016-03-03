2d9d19adef22e228b9f0c83b8d20d72305a0c121
Geoffrey Chandler <geoffc@gmail.com>
2016-03-02 17:40:12 -0800

    use new plugin for test lifecycle

--- scripts/docker-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/docker-init.sh
-
-docker build -t rbmh-docker.docker.rbmhops.net/$PROJECT_NAME:$PROJECT_VERSION build/
--- scripts/docker-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/docker-init.sh
-
-# Finally we can push the tagged image
-docker push rbmh-docker.docker.rbmhops.net/${PROJECT_NAME}:${PROJECT_VERSION}
