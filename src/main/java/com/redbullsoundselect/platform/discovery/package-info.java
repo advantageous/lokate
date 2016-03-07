170e7f30ca8e451d71bcca5125cfa6a65a9a3225
Geoffrey Chandler <geoffc@gmail.com>
2016-03-07 15:53:01 -0800

    fixed node package name

--- src/main/java/com/redbullsoundselect/platform/discovery/package-info.java
+++ src/main/java/com/redbullsoundselect/platform/discovery/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(groupPackage = "com.redbullsoundselect.platform.discovery", name = "rbmh-discovery-service-client")
+@ModuleGen(groupPackage = "com.redbullsoundselect.platform.discovery", name = "rbss-discovery-service-client")
 package com.redbullsoundselect.platform.discovery;
 
 import io.vertx.codegen.annotations.ModuleGen;
