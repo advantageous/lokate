9ceb74b2d0821228b32897f279624a966c69df4f
RichardHightower <richard.m.hightower@gmail.com>
2016-04-14 13:02:18 -0700

    small changes

--- src/main/java/com/redbullsoundselect/platform/discovery/DiscoveryService.java
+++ src/main/java/com/redbullsoundselect/platform/discovery/DiscoveryService.java
@@ -1,6 +1,20 @@
 package com.redbullsoundselect.platform.discovery;
 
+import com.redbullmediahouse.platform.config.ConfigUtils;
+import com.redbullsoundselect.platform.discovery.impl.AmazonEc2DiscoveryService;
+import com.redbullsoundselect.platform.discovery.impl.DnsDiscoveryServiceUsingARecords;
+import com.redbullsoundselect.platform.discovery.impl.DockerDiscoveryService;
+import com.redbullsoundselect.platform.discovery.impl.MarathonDiscoveryService;
+import com.typesafe.config.Config;
 import io.advantageous.qbit.reactive.Callback;
+import io.vertx.core.Vertx;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Look up a service by name.
@@ -20,6 +34,35 @@ public interface DiscoveryService {
     }
 
     default void close() {
+    }
+
+
+    static DiscoveryService createDiscoveryService(final Vertx vertx) {
+        final Config discoveryConfig = ConfigUtils.getConfig("com.redbullsoundselect.platform.discovery");
+        final List<String> providers = discoveryConfig.getStringList("providers");
+        final Map<String, Function<Config, DiscoveryService>> discoveryServiceProviders =
+                new HashMap<String, Function<Config, DiscoveryService>>() {
+                    {
+                        /* Docker. */
+                        this.put("docker", cfg ->
+                                new DockerDiscoveryService(vertx,
+                                        URI.create(cfg.getString("host")).getHost(),
+                                        cfg.getInt("port")));
+                        /* DNS */
+                        this.put("dns-basic", cfg -> new DnsDiscoveryServiceUsingARecords(vertx, cfg));
+                        /* Marathon */
+                        this.put("marathon", cfg -> new MarathonDiscoveryService(vertx, cfg));
+                        /* Amazon */
+                        this.put("amazon-ec2", cfg -> new AmazonEc2DiscoveryService(vertx, cfg));
+                    }
+                };
+
+        final List<DiscoveryService> services = providers.stream().map(providerName ->
+                discoveryServiceProviders.get(providerName)
+                        .apply(discoveryConfig.getConfig("provider-config." + providerName)))
+                .collect(Collectors.toList());
+
+        return new DiscoveryServiceImpl(services.toArray(new DiscoveryService[services.size()]));
     }
 
 }
