7e79fe9c470d4c54f2f7d863a7b5d1c85eff435e
Geoffrey Chandler <geoffc@gmail.com>
2016-04-24 15:20:05 -0700

    - removed default methods in ServiceDiscovery interface for random and first services.  this logic should be in the consumer client code

--- src/main/java/com/redbullsoundselect/platform/discovery/DiscoveryService.java
+++ src/main/java/com/redbullsoundselect/platform/discovery/DiscoveryService.java
@@ -4,7 +4,6 @@ import io.advantageous.reakt.promise.Promise;
 
 import java.net.URI;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Look up a service with a discovery URI.
@@ -13,17 +12,8 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public interface DiscoveryService {
 
-    ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
     String QUERY_SCHEME = "discovery";
     String RESULT_SCHEME = "service";
-
-    default Promise<URI> lookupRandomService(URI query) {
-        return lookupService(query).thenMap(list -> list.get(Math.abs(RANDOM.nextInt()) % (list.size() - 1)));
-    }
-
-    default Promise<URI> lookupFirstService(URI query) {
-        return lookupService(query).thenMap(list -> list.get(0));
-    }
 
     default Promise<List<URI>> lookupService(String query) {
         return lookupService(URI.create(query));
